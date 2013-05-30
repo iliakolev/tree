8f9ed18492cad8f8a6b72688d3a57ec7e5b3e02d
Ilia Kolev <ikolev@jinxfactory.com>
2013-05-30 16:58:53 +0300

    remove extra code

--- config.rb
+++ config.rb
@@ -45,9 +45,3 @@ Sass::Script::Number.precision = 7
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass src scss && rm -rf sass && mv scss sass

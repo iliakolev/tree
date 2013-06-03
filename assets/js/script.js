f81eb036bf78e9e7872fc49e5c84ae9f6bbb7a64
Ilia Kolev <ikolev@jinxfactory.com>
2013-06-03 12:43:15 +0300

    Clear empty space, semicolon missing

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,9 @@ jQuery(document).ready(function($)
 	//	closure and available to all methods.
 	//	Separate words with underscores.
 	/***********************************************/
-	
+
 	var CONSTANT_NAME = 'A constant, available to all methods of this closure.';
-	
+
 
 	/***********************************************/
 	//	VARS
@@ -22,26 +22,26 @@ jQuery(document).ready(function($)
 	//	closure and available to all methods.
 	//	Use camel case to separate words.
 	/***********************************************/
-	
-	var instanceVariable = 'An instance variable, available to all methods of this closure.';	
-	
-	
+
+	var instanceVariable = 'An instance variable, available to all methods of this closure.';
+
+
 	/***********************************************/
 	//	INIT
 	//	This function will be called upon load, so call
-	//	any initialastion functions here. 
+	//	any initialastion functions here.
 	/***********************************************/
-	
+
 	this.init = function()
-	{	
-		getData();	
+	{
+		getData();
 	}();
 
 
 	/***********************************************/
 	//	EXAMPLE METHODS
-	/***********************************************/	
-	
+	/***********************************************/
+
 	// get some data
 	// uncomment the lines below to test functionality
 	function getData()
@@ -57,18 +57,17 @@ jQuery(document).ready(function($)
 		// call a method - maybe an ajax call?
 		//onData('onData: I was called from on getData.');
 	}
-	
+
 	// on data return
 	function onData(data)
-	{  		
+	{
 		// log out some data
 		console.log(data);
-		
+
 		// get the body with jQuery, do something with it.
 		$('body').each(function (){
 			console.log(this);
-		})
-	}	
-		
-});
+		});
+	}
 
+});

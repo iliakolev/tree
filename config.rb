1fdeb93e86b1d5798ab1a92dfd34af5697412051
Ilia Kolev <contact@iliakolev.com>
2013-07-06 22:48:48 +0300

    enable css minify as default

--- config.rb
+++ config.rb
@@ -25,7 +25,7 @@ fonts_dir = "assets/fonts"
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 # Over-ride with force compile to change output style with: compass compile --output-style compressed --force
-output_style = :expanded
+output_style = :compressed
 
 
 # Remove SASS/Compass relative comments.

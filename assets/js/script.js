266e71fa7ea4e2a609d50385b05ff51bc03e8324
Ilia Kolev <contact@iliakolev.com>
2013-07-07 16:34:53 +0300

    convert identation to spaces

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,70 +4,70 @@
 jQuery(document).ready(function($)
 {
 
-	/***********************************************/
-	//	CONSTANTS
-	//	Use upper case variable names to declare
-	//	constants such as configuration paths, scoped to this
-	//	closure and available to all methods.
-	//	Separate words with underscores.
-	/***********************************************/
+    /***********************************************/
+    //  CONSTANTS
+    //  Use upper case variable names to declare
+    //  constants such as configuration paths, scoped to this
+    //  closure and available to all methods.
+    //  Separate words with underscores.
+    /***********************************************/
 
-	var CONSTANT_NAME = 'A constant, available to all methods of this closure.';
-
-
-	/***********************************************/
-	//	VARS
-	//	Use lower case variable names to declare
-	//	variables that are scoped to this
-	//	closure and available to all methods.
-	//	Use camel case to separate words.
-	/***********************************************/
-
-	var instanceVariable = 'An instance variable, available to all methods of this closure.';
+    var CONSTANT_NAME = 'A constant, available to all methods of this closure.';
 
 
-	/***********************************************/
-	//	INIT
-	//	This function will be called upon load, so call
-	//	any initialastion functions here.
-	/***********************************************/
+    /***********************************************/
+    //  VARS
+    //  Use lower case variable names to declare
+    //  variables that are scoped to this
+    //  closure and available to all methods.
+    //  Use camel case to separate words.
+    /***********************************************/
 
-	this.init = function()
-	{
-		getData();
-	}();
+    var instanceVariable = 'An instance variable, available to all methods of this closure.';
 
 
-	/***********************************************/
-	//	EXAMPLE METHODS
-	/***********************************************/
+    /***********************************************/
+    //  INIT
+    //  This function will be called upon load, so call
+    //  any initialastion functions here.
+    /***********************************************/
 
-	// get some data
-	// uncomment the lines below to test functionality
-	function getData()
-	{
-		// define a local variable
-		var localVariable = 'A local variable, scoped to this method.';
-		//console.log(localVariable);
+    this.init = function()
+    {
+        getData();
+    }();
 
-		// access some instance variables
-		//console.log(CONSTANT_NAME);
-		//console.log(instanceVariable);
 
-		// call a method - maybe an ajax call?
-		//onData('onData: I was called from on getData.');
-	}
+    /***********************************************/
+    //  EXAMPLE METHODS
+    /***********************************************/
 
-	// on data return
-	function onData(data)
-	{
-		// log out some data
-		console.log(data);
+    // get some data
+    // uncomment the lines below to test functionality
+    function getData()
+    {
+        // define a local variable
+        var localVariable = 'A local variable, scoped to this method.';
+        //console.log(localVariable);
 
-		// get the body with jQuery, do something with it.
-		$('body').each(function (){
-			console.log(this);
-		});
-	}
+        // access some instance variables
+        //console.log(CONSTANT_NAME);
+        //console.log(instanceVariable);
+
+        // call a method - maybe an ajax call?
+        //onData('onData: I was called from on getData.');
+    }
+
+    // on data return
+    function onData(data)
+    {
+        // log out some data
+        console.log(data);
+
+        // get the body with jQuery, do something with it.
+        $('body').each(function (){
+            console.log(this);
+        });
+    }
 
 });
